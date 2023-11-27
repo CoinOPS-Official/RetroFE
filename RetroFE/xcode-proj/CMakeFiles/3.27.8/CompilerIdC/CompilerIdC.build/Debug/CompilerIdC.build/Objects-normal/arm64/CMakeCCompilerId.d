6cd48aea1816054fde864302aed5e3a94a46df5d
aidenjbass <moshidosh83@gmail.com>
2023-11-27 19:03:26 +0000

    added xcode project

---
+++ RetroFE/xcode-proj/CMakeFiles/3.27.8/CompilerIdC/CompilerIdC.build/Debug/CompilerIdC.build/Objects-normal/arm64/CMakeCCompilerId.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/aiden/Documents/Github/RetroFE/RetroFE/xcode-proj/CMakeFiles/3.27.8/CompilerIdC/CMakeCCompilerId.c
---
+++ RetroFE/xcode-proj/CMakeFiles/3.27.8/CompilerIdCXX/CompilerIdCXX.build/Debug/CompilerIdCXX.build/Objects-normal/arm64/CMakeCXXCompilerId.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/aiden/Documents/Github/RetroFE/RetroFE/xcode-proj/CMakeFiles/3.27.8/CompilerIdCXX/CMakeCXXCompilerId.cpp
