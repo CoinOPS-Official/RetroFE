3f8ae8b2caa3fb5ec11188f0b82e6e5b311ca404
inigomonyota <96964704+inigomonyota@users.noreply.github.com>
2024-02-01 09:39:56 -0500

    Configuration::HardwareVideoAccel not !Configuration::HardwareVideoAccel

--- RetroFE/Source/Graphics/Page.cpp
+++ RetroFE/Source/Graphics/Page.cpp
@@ -47,7 +47,7 @@ Page::Page(Configuration &config, int layoutWidth, int layoutHeight)
     , selectSoundChunk_(NULL)
     , minShowTime_(0)
     , jukebox_(false)
-    , useThreading_(SDL::getRendererBackend(0) != "opengl" || !Configuration::HardwareVideoAccel)
+    , useThreading_(SDL::getRendererBackend(0) != "opengl" || Configuration::HardwareVideoAccel)
 {
 
     for (int i = 0; i < MAX_LAYOUTS; i++)
