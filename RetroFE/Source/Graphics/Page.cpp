1cb4d56421424b435c2c0f5bafa51e8a10167116
inigomonyota <96964704+inigomonyota@users.noreply.github.com>
2024-02-02 21:11:28 -0500

    corrected if statement for useThreading_

--- RetroFE/Source/Graphics/Page.cpp
+++ RetroFE/Source/Graphics/Page.cpp
@@ -47,11 +47,7 @@ Page::Page(Configuration &config, int layoutWidth, int layoutHeight)
     , selectSoundChunk_(NULL)
     , minShowTime_(0)
     , jukebox_(false)
-#ifdef WIN32
-    , useThreading_(SDL::getRendererBackend(0) != "opengl" || Configuration::HardwareVideoAccel)
-#else
-    , useThreading_(false)
-#endif
+    , useThreading_(SDL::getRendererBackend(0) != "opengl" && !Configuration::HardwareVideoAccel)
 {
 
     for (int i = 0; i < MAX_LAYOUTS; i++)
