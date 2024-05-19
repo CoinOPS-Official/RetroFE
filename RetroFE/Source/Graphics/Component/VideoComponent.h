bae84b300e20026541a98ac9cde3525630c1577b
inigomonyota <96964704+inigomonyota@users.noreply.github.com>
2024-05-19 11:31:24 -0400

    mark VideoComponent class final

--- RetroFE/Source/Graphics/Component/VideoComponent.h
+++ RetroFE/Source/Graphics/Component/VideoComponent.h
@@ -23,11 +23,11 @@
 #include <SDL2/SDL.h>
 #include <string>
 
-class VideoComponent : public Component
+class VideoComponent final : public Component
 {
 public:
     VideoComponent(Page &p, const std::string& videoFile, int monitor, int numLoops);
-    ~VideoComponent() final;
+    ~VideoComponent() override;
     bool update(float dt) override;
     void draw() override;
     void freeGraphicsMemory() override;
