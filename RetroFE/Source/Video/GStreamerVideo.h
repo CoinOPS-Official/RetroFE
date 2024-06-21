ea1cdc10c5a6e6df96e18bb63bce03d3414b5d6a
inigomonyota <96964704+inigomonyota@users.noreply.github.com>
2024-06-21 12:28:40 -0400

    header cleanup

--- RetroFE/Source/Video/GStreamerVideo.h
+++ RetroFE/Source/Video/GStreamerVideo.h
@@ -1,31 +1,15 @@
-/* This file is part of RetroFE.
- *
- * RetroFE is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * RetroFE is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with RetroFE.  If not, see <http://www.gnu.org/licenses/>.
- */
 #pragma once
 
 #include "../Database/Configuration.h"
 #include "../SDL.h"
 #include "../Utility/Utils.h"
 #include "IVideo.h"
-#include <condition_variable>
 #include <atomic>
+#include <future>
 #include <mutex>
 #include <queue>
 #include <shared_mutex>
 #include <thread>
-#include <future>
 
 extern "C"
 {
@@ -34,9 +18,7 @@ extern "C"
 #include <GStreamer/gst/video/video.h>
 #else
 #include <gst/gst.h>
-#include <gst/pbutils/pbutils.h>
 #include <gst/video/video.h>
-
 #endif
 }
 
@@ -47,6 +29,8 @@ class GStreamerVideo final : public IVideo
     GStreamerVideo(const GStreamerVideo &) = delete;
     GStreamerVideo &operator=(const GStreamerVideo &) = delete;
     ~GStreamerVideo() override;
+
+    // IVideo interface methods
     bool initialize() override;
     bool play(const std::string &file) override;
     bool stop() override;
@@ -72,52 +56,62 @@ class GStreamerVideo final : public IVideo
     bool isPaused() override;
 
   private:
-    static void processNewBuffer(GstElement const * /* fakesink */, GstBuffer *buf, GstPad *new_pad, gpointer userdata);
-    static void elementSetupCallback([[maybe_unused]] const GstElement& playbin, GstElement* element,
-        [[maybe_unused]] GStreamerVideo* video);
-    static GstPadProbeReturn padProbeCallback(GstPad* pad, GstPadProbeInfo* info, gpointer user_data);
-    static void enablePlugin(const std::string& pluginName);
-    static void disablePlugin(const std::string& pluginName);
+    // GStreamer callback methods
+    static void processNewBuffer(GstElement const *fakesink, GstBuffer *buf, GstPad *new_pad, gpointer userdata);
+    static void elementSetupCallback(const GstElement &playbin, GstElement *element, GStreamerVideo *video);
+    static GstPadProbeReturn padProbeCallback(GstPad *pad, GstPadProbeInfo *info, gpointer user_data);
+
+    // GStreamer utility methods
+    static void enablePlugin(const std::string &pluginName);
+    static void disablePlugin(const std::string &pluginName);
     bool initializeGstElements(const std::string &file);
     void createSdlTexture();
     void clearBuffers();
-    GstElement *playbin_{nullptr};
-    GstElement *videoSink_{nullptr};
-    GstBus *videoBus_{nullptr};
+
+    // GStreamer elements
+    GstElement *playbin_ = nullptr;
+    GstElement *videoSink_ = nullptr;
+    GstBus *videoBus_ = nullptr;
     GstVideoInfo videoInfo_;
-    bool videoInfoSet_{false};
-    SDL_Texture *texture_{nullptr};
-    SDL_PixelFormatEnum sdlFormat_{SDL_PIXELFORMAT_UNKNOWN};
-    guint elementSetupHandlerId_{0};
-    guint handoffHandlerId_{0};
-    guint padProbeId_{0};
-    gint height_{0};
-    gint width_{0};
-    
-    std::atomic<bool> isPlaying_{false};
-    
+    bool videoInfoSet_ = false;
+
+    // SDL elements
+    SDL_Texture *texture_ = nullptr;
+    SDL_PixelFormatEnum sdlFormat_ = SDL_PIXELFORMAT_UNKNOWN;
+
+    // GStreamer handler IDs
+    guint elementSetupHandlerId_ = 0;
+    guint handoffHandlerId_ = 0;
+    guint padProbeId_ = 0;
+
+    // Video properties
+    gint height_ = 0;
+    gint width_ = 0;
+
+    // Atomic flags
+    std::atomic<bool> isPlaying_ = false;
     static std::atomic<bool> initialized_;
     static std::once_flag initFlag_;
+    std::atomic<bool> paused_ = false;
+    std::atomic<bool> frameReady_ = false;
+    std::atomic<bool> bufferQueueEmpty_ = true;
+    std::atomic<bool> stopping_ = false;
 
-    int playCount_{0};
-    std::string currentFile_{};
-    int numLoops_{0};
-    float volume_{0.0f};
-    double currentVolume_{0.0};
-    int monitor_;
-    std::atomic<bool> paused_{false};
-    double lastSetVolume_{0.0};
-    bool lastSetMuteState_{false};
-    
-    std::atomic<bool> frameReady_{false};
-    std::atomic<bool> bufferQueueEmpty_{true};
-    std::atomic<bool> stopping_{false};
-    
+    // Synchronization
     std::mutex syncMutex_;
-
-    std::queue<GstBuffer*> bufferQueue_;
-
+    std::queue<GstBuffer *> bufferQueue_;
     std::future<void> stopFuture_;
 
+    // Other properties
+    int playCount_ = 0;
+    std::string currentFile_;
+    int numLoops_ = 0;
+    float volume_ = 0.0f;
+    double currentVolume_ = 0.0;
+    int monitor_;
+    double lastSetVolume_ = 0.0;
+    bool lastSetMuteState_ = false;
+
+    // Utility methods
     std::string generateDotFileName(const std::string &prefix, const std::string &videoFilePath) const;
 };
