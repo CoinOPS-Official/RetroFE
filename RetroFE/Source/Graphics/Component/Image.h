b218a2ba2344a81e5d632aeb3ab943f96c484858
aidenjbass <72824515+aidenjbass@users.noreply.github.com>
2025-03-29 21:10:25 +0000

    Update Image.h

--- RetroFE/Source/Graphics/Component/Image.h
+++ RetroFE/Source/Graphics/Component/Image.h
@@ -23,7 +23,11 @@
 #include <SDL2_image/SDL_image.h>
 #include <webp/decode.h>
 #include <webp/demux.h>
-#else
+#elif defined(_WIN32) 
+#include <SDL_image.h>
+#include <decode.h>
+#include <demux.h>
+#else  // Assume Linux
 #include <SDL_image.h>
 #include <webp/decode.h>
 #include <webp/demux.h>
