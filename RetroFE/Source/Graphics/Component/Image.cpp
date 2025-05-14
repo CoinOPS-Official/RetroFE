683492b85a76c5dbd49b93fef1f2dfd636f058c5
inigomonyota <96964704+inigomonyota@users.noreply.github.com>
2025-05-14 16:30:22 -0400

    existance check

--- RetroFE/Source/Graphics/Component/Image.cpp
+++ RetroFE/Source/Graphics/Component/Image.cpp
@@ -44,6 +44,7 @@
 #include <vector>
 #include <cstring>
 #include <optional>
+#include <filesystem>
 
  // Definition of static members
 Image::PathCache Image::pathCache_;
@@ -148,9 +149,10 @@ void Image::allocateGraphicsMemory() {
         return success;
         };
 
-    if (tryLoad(file_))
+    if (!file_.empty() && std::filesystem::exists(file_) && tryLoad(file_))
         return;
-    if (!altFile_.empty() && tryLoad(altFile_))
+
+    if (!altFile_.empty() && std::filesystem::exists(altFile_) && tryLoad(altFile_))
         return;
     LOG_ERROR("Image", "Failed to load both primary and alternative image files: " + file_ + " | " + altFile_);
 }
